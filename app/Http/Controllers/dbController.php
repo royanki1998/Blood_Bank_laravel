efe77eca4176d06cf028f454b9549fc5faaf78fa
Anirban0 <deanirban74@Gmail.com>
2022-12-26 20:56:17 +0530

    Database setup

--- app/Http/Controllers/dbController.php
+++ app/Http/Controllers/dbController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\donor;
+use App\Models\recipient;
+use App\Models\blood;
+
 
 class dbController extends Controller
 {
@@ -11,5 +14,16 @@ class dbController extends Controller
     {
         $data = donor::paginate(5);
         return view('donorTable',['donors'=>$data]);
+    }
+
+    function dbRecipient()
+    {
+        $data = recipient::paginate(5);
+        return view('recipientTable',['recipients'=>$data]);
+    }
+    function dbBlood()
+    {
+        $data = blood::paginate(5);
+        return view('bloodTable',['bloods'=>$data]);
     }
 }
